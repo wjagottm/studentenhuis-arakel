a3401874227155cc74bdbbf91dc5a665cdd4ea53
Kelly Premchuen <vpremchu@avans.nl>
2018-05-15 22:04:58 +0200

    Aantal testen geschreven Raamwerk van de controllers gemaakt

---
+++ controllers/deelnemer_controller.js
@@ -0,0 +1,28 @@
+//
+//CRUD operaties op Deelnemer
+//
+let Deelnemer = require('../models/Deelnemer')
+const assert = require('assert')
+
+var db = require('../config/db');
+
+let deelnemerlist = []
+
+module.exports = {
+    createDeelnemer(req, res, next) {
+
+    },
+
+    getDeelnemer(req, res, next) {
+
+    },
+
+    editDeelnemer(req, res, next) {
+
+    },
+
+    deleteDeelnemer(req, res, next) {
+
+    }
+
+}
---
+++ controllers/maaltijd_controller.js
@@ -0,0 +1,28 @@
+//
+//CRUD operaties op maaltijd
+//
+let Maaltijd = require('../models/Maaltijd')
+const assert = require('assert')
+
+var db = require('../config/db');
+
+let maaltijdlist = []
+
+module.exports = {
+    createMaaltijd(req, res, next) {
+
+    },
+
+    getMaaltijd(req, res, next) {
+
+    },
+
+    editMaaltijd(req, res, next) {
+
+    },
+
+    deleteMaaltijd(req, res, next) {
+
+    }
+
+}
